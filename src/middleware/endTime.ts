27ee450133082af95a0cd125b12c048227404dcb
AlfieriChou <alfierichou@gmail.com>
2018-11-07 09:23:31 +0800

    add end time

--- src/middleware/endTime.ts
+++ src/middleware/endTime.ts
@@ -1,9 +1,9 @@
 import { KoaMiddlewareInterface, Middleware } from 'routing-controllers'
 
-@Middleware({ type: 'before' })
+@Middleware({ type: 'after' })
 export class StartTimeMiddleware implements KoaMiddlewareInterface {
   use(ctx: any, next: (err?: any) => Promise<any>): any {
-    console.log('-------> ' + ctx.request.method + ' ' + ctx.request.url)
+    console.log('<------- ' + ctx.response.status + ' ' + ctx.request.method + ' ' + ctx.request.url)
     next()
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import { createKoaServer } from 'routing-controllers'
 import { UserController } from './controller/user'
 import './middleware/startTime'
+import './middleware/endTime'
 
 const app = createKoaServer({
    controllers: [UserController]
