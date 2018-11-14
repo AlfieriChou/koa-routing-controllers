062497fa982c617defaa0f0527bed4927f79b561
AlfieriChou <alfierichou@gmail.com>
2018-11-14 12:35:46 +0800

    jsonwebtoken

--- src/controller/account.ts
+++ src/controller/account.ts
@@ -4,6 +4,8 @@ import { Account } from '../entity/account'
 import { getManager } from 'typeorm'
 import { BaseController } from '../common/baseController'
 import { IsString, Length } from 'class-validator'
+import * as bcrypt from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
 
 class LoginSchema {
   @IsString()
@@ -15,6 +17,17 @@ class LoginSchema {
 
 @Controller()
 export class AccountController extends BaseController {
+
+  private createToken (user: any) {
+    let end: Date = new Date()
+    end.setDate(end.getDate() + 1)
+    let timeStamp: number = Date.parse(end.toString())
+    return jwt.sign({
+      sub: {user},
+      exp: timeStamp
+    }, 'koa-routing')
+  }
+
   @Get('/accounts')
   @OpenAPI({ summary: '获取account信息列表' })
   @ResponseSchema(Account, {
@@ -30,6 +43,14 @@ export class AccountController extends BaseController {
   async login (
     @Body({ required: true }) params: LoginSchema
   ) {
-    return params
+    const user: any = await super.exists('Account', { username: params.username })
+    if (bcrypt.compare(params.password, user.password)) {
+      const result = this.createToken(user)
+      return {
+        token: result
+      }
+    } else {
+      return '登录失败'
+    }
   }
 }
